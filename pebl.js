a41ea8741a2d4418d834436c66b26aea46954a5e
Cedric ( z.) Le Berger <cedricoop@netscape.net>
2014-09-29 21:33:48 +0100

    The first commit of pebljs yeaaaaah !!

---
+++ pebl.js
@@ -0,0 +1,194 @@
+/***
+ * 
+ * * pebl.js
+ * 
+ * simple js templating
+ * 
+ * @author Cedric, hello@linux-commands-examples.com
+ * @date 24 Jan 2013, modified 29 Sept 2014
+ * 
+ * @requires jQuery
+ * 
+ * It replaces all data the dataElement has in the original element
+ * dataElement is an key=>value array
+ * 
+ * for each key : '{'+key+'}' will be replaced in the original template (html) element
+ * 
+ * dataProvider is an url, loadData will get it,
+ *  the ajax call should return an array with key=>values element that match
+ *   the original html template
+ * 
+ * usage : 
+ //create an object
+ tpl = new Pebl($('#tpl'), '/_category/help-categorise-ajax', _events);
+ //load data
+ tpl.loadData();  
+ //replace
+ tpl.replace();
+ *
+ ***/
+
+/**
+ * 
+ * @param jQueryElement tplElement
+ * @param string url == dataProvider
+ * @param function events to bind. When replacing the content,
+ *   events are not bound anymore. We need to rebind them man
+ * @returns Pebl object
+ */
+function Pebl(tplElement, url, events) {
+  this.dataProvider = url;
+  var dataElement = [];
+  this.tplElement = tplElement;
+  this.events = events;
+  this.tplOriginalHtml = tplElement.html();
+  if ('undefined' === typeof this.tplOriginalHtml)
+    throw 'Make sure that the tplElement exists and is not empty';
+
+  /**
+   * @returns Pebl object
+   */
+  this.replace = function() {
+    var oneDataPiece = this.shiftData();
+    var html = this.tplOriginalHtml;
+    html = replaceRecurisive(html, oneDataPiece);
+    this.tplElement.html(html);
+    this.bindEvents();
+    return this;
+  }
+
+  var replaceArray = function(key, html, oneData) {
+    var htmlResult = '';
+    var htmlOfSubPart;
+    var regx = new RegExp('{' + key + '/}' + '([\\s\\S]*)' + '{/' + key + '}', 'm');
+    htmlOfSubPart = html.match(regx);
+    if (htmlOfSubPart && null !== typeof htmlOfSubPart[1]) {
+      htmlOfSubPart = htmlOfSubPart[1];
+      for (var oneDataKey in oneData) {
+        if (oneData.hasOwnProperty(oneDataKey)) {
+          htmlResult += replaceRecurisive(htmlOfSubPart, oneData[oneDataKey]);
+        }
+        else {
+          console.warn('hu ho ! This shoud not happen');
+        }
+      }
+      var regx = new RegExp('{' + key + '/}' + '[\\s\\S]*' + '{/' + key + '}');
+      html = html.replace(regx, htmlResult);
+    }
+    else {
+      html = '';
+      console.warn('no html template element was found for"' + key + '"');
+    }
+    return html;
+  }
+
+  var replaceRecurisive = function(html, oneDataPiece) {
+    for (var key in oneDataPiece) {
+      if (oneDataPiece.hasOwnProperty(key)) {
+        try {
+          var oneData = oneDataPiece[key];
+
+          if (oneData instanceof Array) {
+            html = replaceArray(key, html, oneData)
+          }
+          else {
+            var regx = new RegExp('{' + key + '}', 'g');
+            html = html.replace(regx, oneData ? oneData : '');
+          }
+        } catch (e) {
+          console.warn(e.message);
+        }
+      }
+      else {
+        console.warn('Man, why, why so many zombies disguised in Santa Claus?');
+      }
+    }
+    return html || '';
+  }
+
+  /**
+   * @returns Pebl object
+   */
+  this.bindEvents = function() {
+    //@todo : only apply that to the element replaced,
+    // so that it is localised
+    if ('function' === typeof this.events) {
+      this.events();
+    }
+
+    return this;
+  }
+
+  /**
+   * ajax call - used to load new data,
+   *  and to post something (see arg)
+   * @param arg param for get call
+   * @param callback function when successful
+   * @returns Pebl object
+   */
+  this.loadData = function(arg, _callback) {
+    var that = this;
+    $.ajax({
+      url: that.dataProvider,
+      type: "GET",
+      data: arg,
+      success: function(data) {
+        try {
+          that.addData(JSON.parse(data));
+          if (typeof _callback !== 'undefined' && typeof _callback !== 'function' && null !== _callback) {
+            throw '_callback is not a function it is a `' + (typeof _callback) + '` !'
+          }
+          if (typeof _callback === 'function') {
+            _callback();
+          }
+        }
+        catch (e) {
+          console.warn(e.message);
+          console.warn('is data returned in JSON format ?? :\n' + data);
+        }
+      }
+    }).fail(function() {
+      console.warn('failed to download more data');
+    });
+    return this;
+  }
+
+  /**
+   * add data to the data stack
+   * @param json object, data
+   * @returns this
+   */
+  this.addData = function(data) {
+    dataElement.push(data);
+    return this;
+  }
+
+  /**
+   * 
+   * @returns json {Pebl@pro;dataElement@call;shift}
+   */
+  this.shiftData = function() {
+    return dataElement.shift();
+  }
+
+  /**
+   * get the element number i from the data stack
+   * @param int i
+   * @returns json object data
+   */
+  this.getAllData = function(i) {
+    return jQuery.extend(true, {}, dataElement)[0];
+    //    return dataElement.slice();
+  }
+
+  /**
+   * console.log data
+   * @returns Pebl object
+   */
+  this.logData = function() {
+    console.log(dataElement);
+    return this;
+  }
+
+  return this;
+}
